005baca35935b116c0c9f155194f870f2de7c542
MSI\Hiram González <mrhiram1@gmail.com>
2018-09-28 09:54:30 -0600

    Dropdown
    menu

--- js/app.js
+++ js/app.js
@@ -1,5 +1,15 @@
-var toRegister = $('.to-register');
+var toRegister = $('.to-register'),
+    btnLogin = $('.btn-login'),
+    btnLogout = $('#logout');
 toRegister.click(function(){
    $('.cuadro-login').css({"display":"none"}),
    $('.cuadro-registro').css({"display":"block"});
+});
+btnLogin.click(function(){
+    $('#login').css({"display":"none"}),
+    $('.dropbtn').css({"display":"inline-block"});
+});
+btnLogout.click(function(){
+    $('#login').css({"display":"inline-block"}),
+    $('.dropbtn').css({"display":"none"});
 });
