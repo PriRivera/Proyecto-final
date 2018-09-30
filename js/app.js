4da3404230e475ce673fc90c0404d77936fb9975
PriRivera <42950786+PriRivera@users.noreply.github.com>
2018-09-30 12:48:32 -0600

    hover del perfil
    cambios en el hover del perfil y cambios en el text area

--- js/app.js
+++ js/app.js
@@ -13,3 +13,19 @@ btnLogout.click(function(){
     $('#login').css({"display":"inline-block"}),
     $('.dropbtn').css({"display":"none"});
 });
+
+//Enter text area
+var textArea = document.getElementById("textArea");
+textArea.addEventListener("keyup", function(event) {
+  event.preventDefault();
+
+  if (event.keyCode === 13) {
+    //document.getElementById("myBtn").click();
+    console.log(textArea.value);
+  }
+});
+
+function guardarDatos()
+{
+    console.log(textArea.value);
+}
